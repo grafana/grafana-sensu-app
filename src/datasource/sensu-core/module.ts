7619cb442fe589e70744be81b54c7cdb3aed161d
Brian Gann <briangann@users.noreply.github.com>
2019-09-07 17:06:38 +0200

    20190907 fix deprecation (#19)
    * fix deprecation

--- src/datasource/sensu-core/module.ts
+++ src/datasource/sensu-core/module.ts
@@ -1,12 +1,9 @@
-///<reference path="../../../node_modules/grafana-sdk-mocks/app/headers/common.d.ts" />
-
 import { SensuCoreDatasource } from './datasource';
 import { SensuCoreDatasourceQueryCtrl } from './query_ctrl';
 
 class SensuConfigCtrl {
   static templateUrl = 'datasource/sensu-core/partials/config.html';
 }
-
 class SensuQueryOptionsCtrl {
   static templateUrl = 'datasource/sensu-core/partials/query.options.html';
 }
